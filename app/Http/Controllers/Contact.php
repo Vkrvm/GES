b4949c86e9db273ddd8d73c4a13dd2409784eac5
Fady Gamil <fadygamilha@gmail.com>
2024-08-10 14:46:57 +0300

    Add Contact controller

---
+++ app/Http/Controllers/Contact.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use File;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactUS;
+use App\Mail\Employee;
+use App\Mail\Supplier;
+
+class Contact extends Controller
+{
+    public function client(){
+        return view('contact.clientform');
+    }
+
+    public function employee(){
+        return view('contact.hiring');
+    }
+
+    public function supplier(){
+        return view('contact.supplierform');
+    }
+
+    public function newClient(Request $request){
+        $res = Mail::to("mahakram35@gmail.com")->send(new ContactUS($request));
+        
+        if($res){
+            return back()->with('sucsess' , 'Thanks For Your Message');
+        }
+        else{
+            return back()->with('fail' , 'Something Went Wrong');
+        }
+    }
+
+    public function newEmployee(Request $request){
+        $fileName = time() . "." . $request->file('resume')->extension();
+        $request->resume->move('attachment',$fileName);
+        $res = Mail::to("mahakram35@gmail.com")->send(new Employee($request,$fileName));
+        
+        if($res){
+            File::delete('attachment/'.$fileName);
+            return back()->with('sucsess' , 'Thanks For Your Message');
+        }
+        else{
+            File::delete('attachment/'.$fileName);
+            return back()->with('fail' , 'Something Went Wrong');
+        }
+    }
+
+    public function newSupplier(Request $request){
+        $res = Mail::to("mahakram35@gmail.com")->send(new Supplier($request,$fileName));
+        
+        if($res){
+            File::delete('attachment/'.$fileName);
+            return back()->with('sucsess' , 'Thanks For Your Message');
+        }
+        else{
+            File::delete('attachment/'.$fileName);
+            return back()->with('fail' , 'Something Went Wrong');
+        }
+    }
+}
